ccad35eb1e4a5d4dcf921990d6fb9e4f797494e2
Samuel <samuel.osselaer@gmail.com>
2025-06-19 12:28:20 +0200

    installatie project

---
+++ spoder-template-1.21.4/src/client/java/github/spaisuiker/wynnmod/SpoderClient.java
@@ -0,0 +1,10 @@
+package github.spaisuiker.wynnmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpoderClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
